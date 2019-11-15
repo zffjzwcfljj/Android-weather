48c13ead47ca8b202e5c62873ff8d84cba85bb6c
zffjzwcfljj <1601950538@qq.com>
2019-11-15 18:56:10 +0800

    后台自动更新天气

--- app/src/main/java/com/example/weather10/WeatherActivity.java
+++ app/src/main/java/com/example/weather10/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.weather10.gson.Forecast;
 import com.example.weather10.gson.Weather;
+import com.example.weather10.service.AutoUpdateService;
 import com.example.weather10.util.HttpUtil;
 import com.example.weather10.util.Utility;
 
@@ -222,7 +224,10 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
+
 
 
 }
